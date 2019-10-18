4c49da766a9f3e59d3c5905b89de31534a9c111f
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-18 00:51:04 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,10 @@
-name = "Maria"
-greeting_a_person(name)
-# => "Hello Maria"
+  # method name      first_parameter, second_parameter
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Maria", "Ruby")
+# > Hello, Maria. We heard you are a great Ruby programmer.
+ 
+greeting_programmer("Steven", "Elixir")
+# > Hello, Steven. We heard you are a great Elixir programmer.
