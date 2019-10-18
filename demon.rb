fc6159734fd5828192b57eb33fc79918d7723ff1
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-18 00:46:42 +0000

    Automatically backed up by Learn

---
+++ demon.rb
@@ -0,0 +1,5 @@
+count = 0 # A bit of data defined outside the loop
+while count < 3 do # A Boolean expression using the bit of data
+  puts "I am the #{count}, I love to count!" # Work
+# A bit of work that moves the bit of data closer to being false
+end
